a3dcdb8b34878659fcdb2a1aa7de654dece57c95
asdf <cnrrn1817@naver.com>
2024-01-30 17:33:29 +0900

    [파이썬]

--- s12-Class/s08-Class-Inherit-01.py
+++ s12-Class/s08-Class-Inherit-01.py
@@ -3,8 +3,9 @@
 # 상속
 """
 class 클래스이름(부모클래스):
-    ...
+   ... 
 """
+
 #%%
 # 부모 클래스
 class Parent:
@@ -16,19 +17,19 @@ class Parent:
         print(f"name({self.name}), age({self.age})")
         
 #%%
-p = Parent("부모", 67)
-p.info()
+p = Parent("부모", 67) 
+p.info()       
 
 #%%
 
 # 자식 클래스
-class ChildA(Parent): # 자식생성자
-    def __init__(self, name, age):
+class ChildA(Parent):
+    def __init__(self, name, age): # 자식 생성자
         Parent.__init__(self, name, age)
-        
+
 #%%
 
-ca = ChildA('홍길동', 34)
+ca = ChildA('홍길동', 34)        
 ca.info() # 부모의 메소드가 호출
 
 #%%
@@ -36,9 +37,9 @@ ca.info() # 부모의 메소드가 호출
 # 자식 클래스
 class ChildB(Parent):
     def __init__(self, name, age, tel):
-        super().__init__(name, age) 
+        super().__init__(name, age)
         self.tel = tel
-        
+
     def info(self): # 메소드 오버라이딩(재정의)
         # super().info() # 부모 메소드 호출
         # print("전화번호:", self.tel)
@@ -46,8 +47,9 @@ class ChildB(Parent):
         
 #%%
 
-cb = ChildB("전우치", 30, "010-1234-1234")
+cb = ChildB("전우치", 30, "010-1234-5678")        
 cb.info() # 자신의 메소드가 호출
+
 
 #%%
 
@@ -56,18 +58,19 @@ class ChildX(ChildA, ChildB):
     def __init__(self, name, age, tel):
         ChildB.__init__(self, name, age, tel)
         
-cx = ChildX("임꺽정", 45, "010-0007-0007")
+
+cx = ChildX("임꺽정", 45, "010-0007-0007")        
 cx.info()
 
 #%%
 
 # 다중상속
-class ChildY(ChildB, ChildA):
+class ChildY(ChildA, ChildB):
     def __init__(self, name, age):
         ChildA.__init__(self, name, age)
         
     def info(self):
-        ChildA.info(self)
-        
-cy = ChildY("와이맨", 55)
+        ChildA.info(self)            
+
+cy = ChildY("와이맨", 55)        
 cy.info()
